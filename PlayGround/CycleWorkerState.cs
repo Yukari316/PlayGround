bef3648006b80e4cb6c29bba34eb92bbb005977a
Yukari316 <cbgancna@gmail.com>
2021-12-09 18:53:21 +0800

    init

---
+++ PlayGround/CycleWorkerState.cs
@@ -0,0 +1,21 @@
+﻿namespace PlayGround;
+
+public enum CycleWorkerState
+{
+    /// <summary>
+    /// 执行中
+    /// </summary>
+    Running,
+    /// <summary>
+    /// 中断
+    /// </summary>
+    Interrupted,
+    /// <summary>
+    /// 停止
+    /// </summary>
+    Stopped,
+    /// <summary>
+    /// 已销毁
+    /// </summary>
+    Disposed
+}
---
+++ PlayGround/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace PlayGround;
+
+public static class Program
+{
+    //这个东西应该在管理池里,并且应该是ConcurrentDictionary
+    //public static Dictionary<Guid, CycleWorker<int>> Workers = new();
+
+    public static void Main()
+    {
+        var worker = new CycleWorker<int>((status, i) =>
+                                          {
+                                              Console.WriteLine(status.CycleCount);
+                                              Thread.Sleep(1000);
+                                              if (status.CycleCount == 4) throw new Exception("死了啦都你害的啦");
+                                          }, 0, -1);
+        worker.OnWorkerThreadException = exception =>
+                                         {
+                                             Console.WriteLine(exception.Message);
+                                         };
+        //Workers.Add(worker.Id, worker);
+        worker.StartWorker();
+        Console.ReadLine();
+        // worker.InterruptWorker();
+        // Console.ReadLine();
+        // worker.StartWorker();
+        // Console.ReadLine();
+    }
+}
